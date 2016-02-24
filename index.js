dc31ba84e471e839435c9807f528d561299bccf4
Oz <oz@appfigures.com>
2016-02-24 10:54:07 -0500

    Fixed an issue. See https://phabricator.babeljs.io/T7140

--- index.js
+++ index.js
@@ -4,11 +4,12 @@ module.exports = {
 		require('babel-preset-react')
 	],
 	plugins: [
-		require('babel-plugin-transform-class-properties'),
 		// Yep, '.default' is a workaround. Took me a while to figure it out.
 		// In any case, this entire plugin is kind of temporary until
 		// babel makes their official decorators plugin work.
 		require('babel-plugin-transform-decorators-legacy').default,
+		require('babel-plugin-transform-class-properties'),
+		// The order of the two above ^ is important as per https://phabricator.babeljs.io/T7140
 		require('babel-plugin-transform-object-rest-spread')
 	]
 }
