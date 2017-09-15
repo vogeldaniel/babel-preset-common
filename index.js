5ad1ca07ab254863f05803d9df381668456b7e0b
Oz <oz@appfigures.com>
2017-09-15 18:48:29 -0400

    Slimming down our deps. I looked into using the 'env' preset but it was about 10s slower to build explorer with it.

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
+// When we move to Babel 7 we'll need to add the flow preset explicitly.
+
 module.exports = {
 	presets: [
 		require('babel-preset-es2015'),
-		require('babel-preset-react'),
-		require('babel-preset-stage-3') // async/await + exponential operator
+		require('babel-preset-react')
 	],
 	plugins: [
+		require('babel-plugin-syntax-trailing-function-commas'),
+		require('babel-plugin-transform-async-to-generator'), // Turns async/await to generators
 		require('babel-plugin-transform-runtime'), // async/await runtime methods
-		require('babel-plugin-transform-inline-environment-variables'),
+		require('babel-plugin-transform-inline-environment-variables'), // So we can do process.env.NODE_ENV
 		// Yep, '.default' is a workaround. Took me a while to figure it out.
 		// In any case, this entire plugin is kind of temporary until
 		// babel makes their official decorators plugin work.
